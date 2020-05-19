3b2f6dc3d3848b36aea8b2b7b693fe0f3dd7f036
userone <lipanchn@163.com>
2020-05-20 00:07:54 +0800

    增加python虚拟环境，修改接口请求页面问题

--- apps/jgpt/views.py
+++ apps/jgpt/views.py
@@ -37,6 +37,10 @@ def menu(request, name):
         return HttpResponse('404 error')
     
 def receiveRecord(request, name):
+    if name == 'interfaceList':
+        json_interfacelist = '{"data":[{"NAME":"1"}],"success":"1"}'
+        print('进入测试interfaceList')
+        return HttpResponse(json.dumps(json.loads(json_interfacelist)),content_type="application/json")
     print(f'请求名字为{name}')
     str_json = '{"total":54,"rows":[{"id":22798,"fromIp":"192.168.190.92","requestUrl":"/imageup/stuimg","requestId":60,"requestTime":"2020-05-19 16:29:37","urlParams":{"v":"1.0.0.e2","sign":"","user":"15BE6CD8362","ts":"1589876953957"},"bodyParams":"","type":"POST","filePath":"http://172.16.8.104:8096/jgpt/wKgAIF3nHJyAP2fKAAAwnDmvjqQ195.jpg","urlRegex":"/imageup/{type}","description":"文件上传接口"}]}'
     print(f'返回的字符串为：{str_json}')
